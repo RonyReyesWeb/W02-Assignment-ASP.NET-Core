0536bf9f144db6bca1d2f08132c503ffe915b591
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2026-09-03 12:28:28 -0700

    adding the initial setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace W02_Assignment_ASP.NET_Core.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
